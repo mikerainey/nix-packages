9262712539582cb9985719f0e35115271ac17e70
mikerainey <bionic.oldsmobile@gmail.com>
2023-05-03 19:54:15 -0400

    edit

--- pkgs/parlaylib-taskparts/default-examples.nix
+++ pkgs/parlaylib-taskparts/default-examples.nix
@@ -1,34 +1,21 @@
-{ stdenv, fetchgit, cmake, taskparts }:
-
-# TODO:
-#  - downloading of input data (optional)
-#  - jemalloc/numactl (optional)
-#  - configurable scheduler
-#  - configurable compiler (needed?)
+{ stdenv, fetchgit, cmake, parlaylibSrc, taskparts }:
 
 stdenv.mkDerivation rec {
 
   name = "parlaylib-examples";
 
-  #####################################################################
-  # src = fetchgit {                                                  #
-  #   url = "https://github.com/cmuparlay/parlaylib.git";             #
-  #   rev = "7cdb4cae8f020525f5eb4ad82e2565d1e38cfbc3";               #
-  #   sha256 = "sha256-ZOmqME4T5Uc9OMDswCj5z6c+I1KG3fBiWIYDMFyeJ74="; #
-  # };                                                                #
-  #####################################################################
-
-  src = ../../../../Scratch/merge/parlaylib;
+  src = parlaylibSrc;
   
   buildInputs = [ cmake taskparts ];
 
+  # for debugging:  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
   configurePhase = ''
     mkdir -p build
-    cmake . -DCMAKE_INSTALL_PREFIX:PATH=$out -DCMAKE_BUILD_TYPE=Release -DPARLAY_EXAMPLES=On -DPARLAY_TASKPARTS=On -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DTASKPARTS_SEARCH_DIR=${taskparts}
+    cmake . -DCMAKE_INSTALL_PREFIX:PATH=$out -DCMAKE_BUILD_TYPE=Release -DPARLAY_EXAMPLES=On -DPARLAY_TASKPARTS=On -DTASKPARTS_SEARCH_DIR=${taskparts}
   '';
 
   buildPhase = ''
-    cmake --build . --target install -j 16
+    cmake --build . --target install -j
     cp -R examples $out/
   '';
   
