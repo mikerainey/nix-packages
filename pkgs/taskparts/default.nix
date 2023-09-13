00122d97aad36b74507801916308ea4ad98cf6ef
mikerainey <bionic.oldsmobile@gmail.com>
2023-09-13 16:58:04 -0400

    extending build script for taskparts to support optional cmake configuration, e.g., hwloc and elastic

--- pkgs/taskparts/default.nix
+++ pkgs/taskparts/default.nix
@@ -1,11 +1,23 @@
-{ stdenv, taskpartsSrc, cmake }:
+{ stdenv, taskpartsSrc, cmake, hwloc, enable-elastic-scheduling ? (hwloc != null) }:
 
 stdenv.mkDerivation rec {
   name = "taskparts";
 
   src = taskpartsSrc;
 
-  buildInputs = [ cmake ];
+  buildInputs = [ cmake ] ++ (if hwloc == null then [] else [ hwloc ]);
+
+  # for debugging:  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
+  configurePhase =
+    let
+      hwloc-cfg = if hwloc == null then "" else "-DHWLOC_DEV_PATH=${hwloc.dev} -DHWLOC_LIB_PATH=${hwloc.lib}";
+      elastic-cfg = if enable-elastic-scheduling then "" else "-DNONELASTIC=ON";
+      cfg = "${hwloc-cfg} ${elastic-cfg}";
+    in
+      ''
+      mkdir -p build
+      cmake . -DCMAKE_INSTALL_PREFIX:PATH=$out -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_BUILD_TYPE=Release -DSTATS=ON ${cfg}
+      '';
 
   meta = {
     description = "A Task-Parallel Run-Time System for C++";
